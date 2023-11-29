24908b518d1410116885acba6a14a137d81cad4e
andyjalex <36079847+andyjalex@users.noreply.github.com>
2023-11-29 19:08:26 +0000

    swagger docs added

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 require('dotenv').config();
 require('express-async-errors');
 
+
 //extra security packages
 const helmet = require('helmet')
 const cors = require('cors')
 const xss = require('xss-clean')
 const rateLimiter = require('express-rate-limit')
+
+// Swagger
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
 
 const express = require('express');
 const app = express();
@@ -29,9 +35,16 @@ app.use(rateLimiter({
   max: 100,
 })
 )
+
 app.use(helmet())
 app.use(cors())
 app.use(xss())
+
+
+app.use('/api-docs',swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+app.get('/', (req, res) => {
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>')
+});
 
 
 // routes
